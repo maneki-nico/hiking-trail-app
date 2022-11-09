29008b8b4ab84f740366766d7c1f7b9e36cda65c
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-09 15:55:16 -0500

    fixed changes

--- app/javascript/react/components/TrailTile.js
+++ app/javascript/react/components/TrailTile.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TrailsTile = (props) => {
     const { trail }  = props
-
-    const [redirect, setRedirect] = useState(false)
 
     const tileClick = () => {
         setRedirect(true)
@@ -12,23 +10,24 @@ const TrailsTile = (props) => {
 
     return (
             <div className="cell small-6 card" onClick={tileClick}>
-                {redirect && <Redirect to={`/trails/${trail.id}`}/>}
-                <div className="card-divider">
-                    <h2>
-                        {trail.name}
-                    </h2>
-                </div>
-                <div className="card-section">
-                    <div className="trail-image">
-                        <img src={trail.image_url} alt={`Image of ${trail.name}.`}/>
+                <Link to={`/trails/${trail.id}`}>
+                    <div className="card-divider">
+                        <h2>
+                            {trail.name}
+                        </h2>
                     </div>
-                    <h4>
-                        {`${trail.city}`}
-                    </h4>
-                    <p>
-                        {`${trail.zip}`}
-                    </p>
-                </div>
+                    <div className="card-section">
+                        <div className="trail-image">
+                            <img src={trail.image_url} alt={`Image of ${trail.name}.`}/>
+                        </div>
+                        <h4>
+                            {trail.city}
+                        </h4>
+                        <p>
+                            {trail.zip}
+                        </p>
+                    </div>
+                </Link>
             </div>
     )
 }
