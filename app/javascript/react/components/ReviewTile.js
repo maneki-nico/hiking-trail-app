5ce913a016aca1dc3f3ae08e307ae48803697b59
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-09 09:33:14 -0500

    review tile/trail show page styling

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -4,9 +4,9 @@ const ReviewTile = (props) => {
     const { review } = props
     return (
         <div className="cell small-4 card review-tile">
-            <h5>{props.username}</h5>
-            <p>Rating: {props.rating}</p>
-            <p>Review: {props.body}</p>
+            <h5>{review.user.username}</h5>
+            <p>Rating: {review.rating}</p>
+            <p>Review: {review.body}</p>
         </div>
     )
 }
