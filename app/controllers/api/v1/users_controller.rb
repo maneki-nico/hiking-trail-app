4750e3a607b1f9598cb8f004e9beba59e2cfbfd0
Nico Foti <nicolefoti@gmail.com>
2022-11-09 12:47:33 -0500

    user review and even MORE styling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApiController
   def show
-    render json: User.find(params[:id])
+    render json: User.find(current_user.id), serializer: UserSerializer, include: ['reviews.trail']
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :zip, :created_at
+  has_many :reviews
 end
