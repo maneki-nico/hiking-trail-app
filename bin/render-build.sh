ee56d2bf9d76b8b94f81eb76d107e737112b3b58
Nico Foti <nicolefoti@gmail.com>
2022-11-27 23:42:19 -0500

    don't drop

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,13 +5,9 @@ set -o errexit
 bundle install
 yarn install
 
-echo $DATABASE_URL
-bundle exec rails runner 'puts ActiveRecord::Base.configurations'
-bundle exec rails runner 'puts ActiveRecord::Base.configurations.inspect'
-
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:drop
+# bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
